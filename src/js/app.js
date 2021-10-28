a2b89a117218f86fe86babbde2b52c08d716abed
devinakabtai <devinmurraydev@gmail.com>
2021-10-28 15:36:21 -0600

    display latest block, not hardcoded gensis block

--- src/js/app.js
+++ src/js/app.js
@@ -42,7 +42,7 @@ App = {
       // This is a known issue with Metamask
       // https://github.com/MetaMask/metamask-extension/issues/2393
       instance.votedEvent({}, {
-        fromBlock: 0,
+        fromBlock: 'latest',
         toBlock: 'latest'
       }).watch(function(error, event) {
         console.log("event triggered", event)
